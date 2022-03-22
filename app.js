2a78d48f357839c5b1fa327a5ba94eff6e920703
Man Ping Chen <manpingchen@gmail.com>
2022-03-22 15:53:44 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "",
+    };
+  },
+  computed: {
+    fullname() {
+      console.log("running again...");
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Chen";
+    },
+  },
+  methods: {
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput() {
+      this.name = "";
+    },
+  },
+});
+
+app.mount("#events");
